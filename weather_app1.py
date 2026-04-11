7179747e501cf10db7e906ec25bbb958726be300
platinumdawg <platinumdawg1@gmail.com>
2026-04-11 20:58:49 +1200

    Update weather_app1.py

--- weather_app1.py
+++ weather_app1.py
@@ -63,10 +63,10 @@ def generate_plot(df):
     fig, ax1 = plt.subplots(figsize=(15, 8))
     ax2 = ax1.twinx()
 
-    ax1.xaxis.set_major_locator(mdates.HourLocator(interval=4))
+    ax1.xaxis.set_major_locator(mdates.HourLocator(interval=6))
     ax1.grid(True, which='major', linestyle='--', alpha=0.3, color='gray')
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%a %d'))
-    plt.xticks(rotation=45)
+    plt.xticks(rotation=90)
     fig.tight_layout() 
     
     ax1.plot(df['time'], df['rain'], color='#44aaff', alpha=0.6, label='Rain (mm/h)')
